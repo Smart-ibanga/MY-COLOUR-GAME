29639bfebff6f2718ccd062e8b37cd11de064972
Smart Ibanga <ibangasmart@gmail.com>
2025-02-07 02:04:22 +0100

    correct ui

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 let colorOption = document.queryselectorAll('.square')
 
 
-
 // Function to generate a random number between min and max (inclusive)
 function generateRandomNumbersBetween(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
